81f41d2f974f474cf93b26182a0d5f9057b59c5d
CSharpFri <simon.radzo@gmail.com>
2018-05-04 21:06:20 +0200

    Add project files.

---
+++ WpfChat/Chat/ContactController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfChat.ChatApp
+{
+    public class ContactController
+    {
+        private Chat _Chat;
+
+        public ContactController(Chat pChat)
+        {
+            _Chat = pChat;
+
+            List<Contact> contacts = new List<Contact>();
+            ContactInfoData data = new ContactInfoData();
+            data._NickName = "Ahoj";
+            contacts.Add(new Contact(data));
+            data._NickName = "Ahoj2";
+            contacts.Add(new Contact(data));
+            data._NickName = "Ahoj3";
+            contacts.Add(new Contact(data));
+            data._NickName = "Ahoj4";
+            contacts.Add(new Contact(data));
+            data._NickName = "Ahoj5";
+            contacts.Add(new Contact(data));
+
+            _Chat.User.AddContacts(contacts);
+        }
+
+        public void SearchContact(string pNickName)
+        {
+
+        }
+
+        public void ChooseContact()
+        {
+
+        }
+
+        public void AddContact(Contact pContact)
+        {
+
+        }
+
+        public void DeleteContact(Contact pContact)
+        {
+
+        }
+
+        public void BlockContact(Contact pContact)
+        {
+
+        }
+
+        public void ReportContact(Contact pContact)
+        {
+
+        }
+
+        public List<Contact> GetContacts()
+        {
+            return _Chat.User.GetContactList();
+        }
+    }
+}
---
+++ WpfChat/WpfDataClasses/WPFContact.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using WpfChat.ChatApp;
+
+namespace WpfChat.WPFDataClasses
+{
+    public class WPFContact : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ContactInfoData _Data;
+        private string _Name;
+
+        public WPFContact()
+        {
+           
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _Data._NickName;
+            }
+            set
+            {
+                this._Name = _Data._NickName;
+                OnPropertyChanged("Name");
+            }
+          
+        }
+
+        public UserStateType ContactStateType
+        {
+            get
+            {
+                return _Data._ContactStateType;
+            }
+        }
+
+        public void SetContactData(ContactInfoData pData)
+        {
+            _Data = pData;
+            this.OnPropertyChanged("Name");
+            //OnPropertyChanged("ContactStateType");
+        }
+
+        private void OnPropertyChanged(string pParam)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            
+            if (handler != null)
+            {
+                handler.Invoke(this, new PropertyChangedEventArgs(pParam));
+            }
+        }
+    }
+}
